36da69d610a7ddb88c20f250ce1fbfd2cd6aa6b6
Maxi Clayton Clowes <maxi@codogo.io>
2018-07-05 00:28:49 +0100

    fix(people): rendering people correctly

--- src/components/common/Banner.js
+++ src/components/common/Banner.js
@@ -1,26 +1,38 @@
 import React, { Component, } from "react";
 import { Carousel, } from "react-responsive-carousel";
+import { siteSettings, } from "src/data";
 
 import styled from "styled-components";
 
-import { siteSettings, } from "src/data";
-
 // --------------------------------
+
+const carouselSettings = {
+	interval: 5000,
+	howArrows: true,
+	showThumbs: false,
+};
 
 class DemoCarousel extends Component {
 	render() {
 		return (
-			<Carousel autoPlay howArrows = { true } showThumbs = { false }>
-				{siteSettings.carousel.map(item => (
-					<div>
-						<img
-							src = { `https://res.cloudinary.com/codogo/image/fetch/h_750,w_1800,c_fill,g_face,f_auto/https:${ item.fields.file.url }` }
-							alt = { item.fields.description }
-						/>
+			<Carousel 
+				autoPlay 
+				interval = { carouselSettings.interval }
+				howArrows = { carouselSettings.howArrows } 
+				showThumbs = { carouselSettings.showThumbs }
+			>
+				{
+					siteSettings.carousel.map(item => (
+						<div>
+							<img
+								src = { `https://res.cloudinary.com/codogo/image/fetch/h_750,w_1800,c_fill,g_face,f_auto/https:${ item.fields.file.url }` }
+								alt = { item.fields.description }
+							/>
 
-						{ item.fields.description && <p className = "legend">{item.fields.description}</p> }
-					</div>
-				))}
+							{ item.fields.description && <p className = "legend">{ item.fields.description }</p> }
+						</div>
+					))
+				}
 			</Carousel>
 		);
 	}
@@ -29,9 +41,9 @@ class DemoCarousel extends Component {
 // --------------------------------
 
 const BannerWrapper = styled.div`
-	width: 100%;
-	position: relative;
 	background: #f6f6f6;
+	position: relative;
+	width: 100%;
 `;
 
 // --------------------------------
