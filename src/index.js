91d84a358fccfacb2df4d2fe471767401fb2e6ed
mcclowes <maxcc@me.com>
2018-10-23 11:14:45 +0100

    fix(import): style component spelled wrong

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import App from "./App";
 import { render, } from "react-snapshot";
+
+import App from "./App";
+import React from "react";
 
 render(<App />, document.getElementById("root"));
 
