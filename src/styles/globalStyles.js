96e5af69cc4a63b7b1c588fa9d51d0d7eef93895
mcclowes <maxcc@me.com>
2019-03-12 11:06:17 +0000

    fix styled components

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,13 +1,13 @@
 import * as mixins from 'codogo-utility-functions';
 import * as vars from './vars';
-import { createGlobalStyle } from 'styled-components';
+import { injectGlobal } from 'styled-components';
 
 // todo: Import this properly
 //import carouselStyles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 // --------------------------------------------------
 
-export default createGlobalStyle`
+export default injectGlobal`
 	@import url('https://fonts.googleapis.com/css?family=Fredoka+One');
 	@import url('https://fonts.googleapis.com/css?family=Raleway:400,600');
 	@import url('https://fonts.googleapis.com/css?family=IM+Fell+English:400i');
