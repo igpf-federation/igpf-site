f544d660b85df6063a33ab57a71f47d9cf56d5e7
mcclowes <maxcc@me.com>
2019-03-12 11:45:56 +0000

    [feat] fix global styles
    fix global styles

--- src/index.js
+++ src/index.js
@@ -4,12 +4,3 @@ import App from './App';
 import React from 'react';
 
 render(<App />, document.getElementById('root'));
-
-// Unregister service workers
-navigator &&
-  navigator.serviceWorker &&
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    registrations.forEach(reg => {
-      reg.unregister();
-    });
-  });
